6f278721d13c1f9593d701c05b1736b687045d0b
Maciej <mackolinop@gmail.com>
2025-03-19 14:09:34 +0100

    Add cosmetics

--- src/main.py
+++ src/main.py
@@ -1,26 +1,39 @@
 import matplotlib.pyplot as plt
 
-file=open("../data/time.txt")
-file2=open("../data/intensity.txt")
-intense=[]
-time=[]
-dayTime=[]
-for i in file:
-    line=i.strip()
-    line=int(line)
+
+time_file = open(
+    "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/time.txt"
+)  # "../data/time.txt"
+intensity_file = open(
+    "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/intensity.txt"
+)  # "../data/intensity.txt"
+
+intense = []
+time = []
+dayTime = []
+
+for i in time_file:
+    line = i.strip()
+    line = int(line)
     time.append(line)
-suma=sum(time)
-avgTime=suma/len(time)
+
+suma = sum(time)
+avgTime = suma / len(time)
 print(avgTime)
 
-for k in file2:
+for k in intensity_file:
     lineIntensity = k.strip()
-    parts = lineIntensity.split('\t')  # Rozdzielenie danych
+    parts = lineIntensity.split("\t")  # Rozdzielenie danych
+
     if len(parts) == 2:
         dayTime.append(int(parts[0]))
-        intense.append(float(parts[1].replace(',', '.')))
-plt.plot(dayTime,intense)
-plt.xlabel("czas w ciągu doby [s]")
-plt.ylabel("ilość połączeń w danej sekundzie")
+        intense.append(float(parts[1].replace(",", ".")))
+
+plt.plot(dayTime, intense)
+
+plt.xlabel("Czas w ciągu doby [s]")
+plt.ylabel("Ilość połączeń w danej sekundzie")
+
 plt.grid()
+
 plt.show()
