23a33dba3a97ba0c69a617d165def43103242cee
Lenovo <qbabuszkiewicz@gmail.com>
2025-03-20 12:00:00 +0100

    upgrade wykresu i liczenie GNR metodą adpqh

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 time_file = open(
-    "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/time.txt"
-)  # "../data/time.txt"
+    #"C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/time.txt"
+ "../data/time.txt")
 intensity_file = open(
-    "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/intensity.txt"
-)  # "../data/intensity.txt"
+    #"C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/intensity.txt"
+  "../data/intensity.txt")
 
 intense = []
 time = []
@@ -29,13 +30,25 @@ for k in intensity_file:
         dayTime.append(int(parts[0]))
         intense.append(float(parts[1].replace(",", ".")))
 
-plt.plot(dayTime, intense)
 
-plt.xlabel("Czas w ciągu doby [s]")
-plt.ylabel("Ilość połączeń w danej sekundzie")
+hourly_intensity = np.zeros(24)
+
+for hour, intensity in zip(dayTime, intense):
+    h = hour // 60
+    hourly_intensity[h] += intensity
+
+hourly_avg = hourly_intensity / 60
+
+peak_h= np.argmax(hourly_avg)
+peak_h_start = peak_h * 60
+peak_h_end = peak_h_start + 60
+
+print(f"Godzina największego ruchu występuje między (ADPQH): {peak_h_start/60}-{peak_h_end/60} h")
+
+plt.plot(dayTime, intense, 'o-')
+
+plt.xlabel("Czas w ciągu doby [min]")
+plt.ylabel("Ilość połączeń w danej minucie")
 
 plt.grid()
-
 plt.show()
-
-print("Elo")
