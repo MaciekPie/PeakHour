e3c2775429fe0e5e9ee24cafb58e57c24e33a537
Maciej <mackolinop@gmail.com>
2025-03-30 15:41:39 +0200

    .

--- src/main.py
+++ src/main.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 time_file = open(
     "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/time.txt"
-)  # "../data/time.txt"
+    # "../data/time.txt"
+)
+
 intensity_file = open(
     "C:/Users/macie/Programowanie/Projekty/Github/PeakHour/data/intensity.txt"
-)  # "../data/intensity.txt"
+    # "../data/intensity.txt"
+)
+
+# domnożyć sobie tabelkę żeby parę dni (6-10)
+# wiele (2){im więcej tym lepiej} metod i pokazac różnice między nimi
+
 
 intense = []
 time = []
 dayTime = []
 
-for i in time_file:
-    line = i.strip()
-    line = int(line)
-    time.append(line)
-
-suma = sum(time)
-avgTime = suma / len(time)
-print(avgTime)
 
 for k in intensity_file:
     lineIntensity = k.strip()
@@ -29,13 +29,27 @@ for k in intensity_file:
         dayTime.append(int(parts[0]))
         intense.append(float(parts[1].replace(",", ".")))
 
-plt.plot(dayTime, intense)
 
-plt.xlabel("Czas w ciągu doby [s]")
-plt.ylabel("Ilość połączeń w danej sekundzie")
+quarter_intensity = np.zeros(96)
+
+for minute, intensity in zip(dayTime, intense):
+    m = minute // 15
+    quarter_intensity[m] += intensity
+
+quarter_avg = quarter_intensity / 15
+
+peak_q = np.argmax(quarter_avg)
+peak_q_start = peak_q * 15
+peak_q_end = peak_q_start + 15
+
+print(
+    f"Kwadrans największego ruchu występuje między (ADPQH): {peak_q_start/60}-{peak_q_end/60} h"
+)
+
+plt.plot(dayTime, intense, "b")
+
+plt.xlabel("Czas w ciągu doby [min]")
+plt.ylabel("Ilość połączeń w danej minucie")
 
 plt.grid()
-
 plt.show()
-
-print("Elo")
